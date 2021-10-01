ea2b2ac0f088532e8107bb37a25666789aaa6fca
dhairya3patel <43616736+dhairya3patel@users.noreply.github.com>
2021-09-30 21:49:22 -0400

    added full topology

--- proj2.fsx
+++ proj2.fsx
@@ -11,3 +11,33 @@ let mutable numNodes = fsi.CommandLineArgs.[1] |> int
 let topology = fsi.CommandLineArgs.[2]
 let algorithm = fsi.CommandLineArgs.[3]
 
+//Creating ActorSystem
+let system = ActorSystem.Create("Project2")
+
+type SupervisorComm = 
+    | Begin of string
+   
+
+type WorkerComm = 
+    | BuildNetwork of string * IActorRef * list<IActorRef> 
+    | Rumour of string 
+    | Terminate of string * IActorRef
+
+// type CommunicationMessages =
+//     | WorkerMessage of int * IActorRef
+//     | EndMessage of IActorRef * string
+//     | SupervisorMessage of int
+//     | CoinMessage of string
+
+
+//Full Topology 
+let createFulltopology (actor:String) (actorList:list<IActorRef>) =
+
+    let id = (actor.Split '-').[1] |> int
+    let mutable neighbourList =[]
+
+    for i in 1..actorList.Length do 
+        if i <> id then
+            neighbourList <- actorList.[i] :: neighbourList
+    neighbourList
+
