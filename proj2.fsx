ae23c883459c888d9d71a950640e69ce198c785f
dhairya3patel <43616736+dhairya3patel@users.noreply.github.com>
2021-10-09 02:35:56 -0400

    added pushsum

--- proj2.fsx
+++ proj2.fsx
@@ -21,7 +21,8 @@ type Comm =
     | BuildNetwork of string * IActorRef * list<IActorRef> 
     | Rumour of int * IActorRef *list<IActorRef>* IActorRef
     | Terminate of string
-    | Acknowledge of IActorRef * list<IActorRef> * int
+    | Receive of float * float * IActorRef * IActorRef
+
 
 //Full Topology 
 let createFulltopology (actor:IActorRef) (actorList:list<IActorRef>) =
@@ -52,12 +53,11 @@ let Gossip (mailbox: Actor<_>) =
     let mutable neighbours = []
     let mutable threshold = 50
     let mutable supervisorRef = mailbox.Self
-    let mutable flag = false
+
     let mutable firstTime = true
     let rec loop () =
         actor {
             let! workermessage = mailbox.Receive()
-            // let workermessage: WorkerComm = message
             match workermessage with
                 |   BuildNetwork(topology,supervisor,actorList) ->                
                         if topology = "full" then 
@@ -91,6 +91,68 @@ let Gossip (mailbox: Actor<_>) =
     loop ()    
 
 
+let Pushsum (mailbox: Actor<_>) =
+    let mutable neighbours = []
+    let mutable supervisorRef = mailbox.Self
+    let mutable s = mailbox.Self.Path.Name.Split('_').[1] |> float
+    let mutable w = 1.0
+    let mutable threshold = 3
+    let mutable ratio = s/w |> float
+    let numCheck = 10.0 ** -10.0 |> float
+    // Console.WriteLine s
+    // Console.WriteLine w
+    // Console.WriteLine numCheck
+    let mutable firstTime = true
+    let rec loop () =
+        actor {
+            let! workermessage = mailbox.Receive()
+            match workermessage with
+                |   BuildNetwork(topology,supervisor,actorList) ->                
+                        if topology = "full" then 
+                            neighbours <- createFulltopology mailbox.Self actorList
+                        else
+                            neighbours <- createLineTopology mailbox.Self actorList
+                        supervisorRef <- supervisor    
+                |   Receive(s_1,w_1,source,supervisorRef) ->
+
+                        s <- s + s_1
+                        w <- w + w_1
+
+                        let newRatio = s/w |> float
+
+                        if not firstTime then
+                            if source <> mailbox.Self then
+                                if abs(newRatio - ratio) < numCheck then 
+                                    threshold <-  threshold - 1
+                                else
+                                   threshold <- 3
+
+                            // Console.WriteLine (mailbox.Self.Path.Name + " Received Again " + threshold.ToString() + "Ratio " + abs(newRatio - ratio).ToString())
+                            if threshold > 0 then
+                                s <- s/2.0
+                                w <- w/2.0
+                                ratio <- newRatio
+                                neighbours.[r.Next(neighbours.Length)] <! Receive(s,w,mailbox.Self,supervisorRef)
+
+                            else if threshold = 0 then
+                                supervisorRef <! Terminate("Down")
+                                Console.WriteLine (mailbox.Self.ToString() + "Down " + threshold.ToString())
+
+                        else 
+                            Console.WriteLine (mailbox.Self.Path.Name + " First " + threshold.ToString() + "Ratio " + abs(newRatio - ratio).ToString())
+                            firstTime <- false
+                            s <- s/2.0
+                            w <- w/2.0
+                            neighbours.[r.Next(neighbours.Length)] <! Receive(s,w,mailbox.Self,supervisorRef)//,received + 1)
+                            system.Scheduler.ScheduleTellRepeatedly(TimeSpan.FromSeconds(0.0),TimeSpan.FromSeconds(0.5),mailbox.Self,Receive(s,w,mailbox.Self,supervisorRef))
+                |   _ -> Console.WriteLine workermessage 
+                        //ignore()
+            return! loop()            
+    }
+
+    loop ()
+
+
 let Supervisor (mailbox: Actor<_>) =
     
     let rec loop () = actor {
@@ -103,14 +165,18 @@ let Supervisor (mailbox: Actor<_>) =
                         //Console.WriteLine actorList
                         actorList |> List.iter(fun node -> node <! BuildNetwork(topology,mailbox.Self,actorList))
                         gossip <- r.Next()
-                        actorList.[r.Next(1,numNodes)] <! Rumour(gossip,mailbox.Self,actorList,mailbox.Self)//,0)
-                        timer.Start()
+                        actorList.[r.Next(1,numNodes)] <! Rumour(gossip,mailbox.Self,actorList,mailbox.Self)
+                    else
+                        let actorList = [ for i in 1 .. numNodes do yield (spawn system ("Actor_" + string (i))) Pushsum]
+                        actorList |> List.iter(fun node -> node <! BuildNetwork(topology,mailbox.Self,actorList))
+                        let selectedActor = actorList.[r.Next(1,numNodes)]
+                        selectedActor <! Receive(selectedActor.Path.Name.Split('_').[1] |> float,1.0,mailbox.Self,mailbox.Self)
 
             |   Terminate(termMsg) -> 
-                    //Console.WriteLine termMsg
+                    Console.WriteLine termMsg
                     if termMsg = "Down" then
                         count <- count + 1
-                        //Console.WriteLine count
+                        Console.WriteLine count
                         if count = numNodes then
                             mailbox.Self <! Terminate("Done")
 
