5a9883c7ccfa32bf6bdab4fd83ecbea4e3526b44
dhairya3patel <43616736+dhairya3patel@users.noreply.github.com>
2021-10-11 01:21:52 -0400

    fixed 3d

--- proj2.fsx
+++ proj2.fsx
@@ -14,6 +14,7 @@ let r = Random()
 let system = ActorSystem.Create("Project2")
 let timer = System.Diagnostics.Stopwatch()
 let mutable dead = []
+let mutable time = 0
 
 type Comm =
     | Begin of string    
@@ -61,30 +62,33 @@ let cubeRootCorrection (number: int)=
     rows
         
 
-
+let seed = cubeRootCorrection(numNodes)
+Console.WriteLine seed
 let create3dTopology (actor:IActorRef) (actorList:list<IActorRef>) =
     let mutable neighbours = []
     let id = (actor.Path.Name.Split '_').[1] |> int
     // let rows = Math.Round(Math.Pow((float numNodes), 0.33)) |> in
-    let seed = cubeRootCorrection(numNodes)
+    
 
     // Console.WriteLine("ID" + id.ToString())
     
-    let seedSquare = seed*seed
-    if id-1 > 0 then 
-        neighbours <- actorList.[id-1] :: neighbours
-    if id-seed > 0 then
-        neighbours <- actorList.[id-seed] :: neighbours
-    if id- (seedSquare) > 0 then
-        neighbours <- actorList.[id-(seedSquare)] :: neighbours
-    if id+1 < numNodes then 
-        neighbours <- actorList.[id+1] :: neighbours
-    if id+seed < numNodes then
-        neighbours <- actorList.[id+seed] :: neighbours
-    if id+ (seedSquare) < numNodes then
-        neighbours <- actorList.[id+(seedSquare)] :: neighbours
+    let seedSquare = seed*seed |> int
+    if id - 1 > 0 then
+        neighbours <- actorList.[id - 2] :: neighbours
+    if id - seed > 0 then
+        neighbours <- actorList.[id - seed - 1] :: neighbours
+    if id - seedSquare > 0 then
+        neighbours <- actorList.[id - seedSquare  - 1] :: neighbours
+    if id + 1 <= numNodes then 
+        neighbours <- actorList.[id] :: neighbours
+    if id + seed <= numNodes then
+        neighbours <- actorList.[id + seed - 1 ] :: neighbours
+    if id + seedSquare <= numNodes then
+        neighbours <- actorList.[id + seedSquare - 1] :: neighbours
 
-    Console.WriteLine(id.ToString() + " " + neighbours.ToString() + " " + neighbours.Length.ToString() )
+    // Console.WriteLine id
+    // for i in neighbours do 
+    //     Console.WriteLine i //(id.ToString() + " " + neighbours.ToString() + " " + neighbours.Length.ToString() )
     neighbours
 
 
@@ -111,12 +115,16 @@ let Gossip (mailbox: Actor<_>) =
                         if not firstTime then
                             // Console.WriteLine (mailbox.Self.Path.Name + " Received Again " + threshold.ToString())
                             
+                            if neighbours.Length = 0 then
+                                if not(List.contains mailbox.Self dead) then
+                                    supervisor <! Terminate("Down",mailbox.Self)
+
                             if source <> mailbox.Self then
                                 threshold <- threshold - 1
                             //    Console.WriteLine (mailbox.Self.Path.Name + " Received Again " + threshold.ToString()) //+ "Count " + count.ToString())
                             if threshold > 0 && neighbours.Length <> 0 then
                                 neighbours.[r.Next(neighbours.Length)] <! Rumour(gossip,mailbox.Self,supervisorRef)//,received)
-                                Console.WriteLine(mailbox.Self.ToString() + "Sending to: " + neighbours.[r.Next(neighbours.Length)].ToString())
+                                //Console.WriteLine(mailbox.Self.ToString() + "Sending to: " + neighbours.[r.Next(neighbours.Length)].ToString())
                                 system.Scheduler.ScheduleTellOnce(TimeSpan.FromSeconds(1.0),mailbox.Self,Rumour(gossip,mailbox.Self,supervisorRef))
                             else if threshold = 0 && source <> mailbox.Self || neighbours.Length = 0 then
                                 if not(List.contains mailbox.Self dead) then
@@ -144,12 +152,12 @@ let Gossip (mailbox: Actor<_>) =
                                             if actorId <> temp then
                                                 newList <- i :: newList
                                         neighbours <- newList
-                                        // if neighbours.Length = 0 then
-                                        //     Terminate("Down",mailbox.Self) |> ignore
+                                        if neighbours.Length = 0 then
+                                            Terminate("Down",mailbox.Self) |> ignore
                                         //Console.WriteLine (mailbox.Self.ToString() + " " + neighbours.Length.ToString())
                                     // Console.WriteLine (List.contains actor neighbours)
-                |   _ -> Console.WriteLine workermessage 
-                        //ignore()
+                |   _ -> //Console.WriteLine workermessage 
+                        ignore()
             return! loop()            
     }
 
@@ -256,16 +264,18 @@ let Supervisor (mailbox: Actor<_>) =
                         actorList |> List.iter(fun node -> node <! BuildNetwork(topology,mailbox.Self,actorList))
                         gossip <- r.Next()
                         actorList.[r.Next(1,numNodes)] <! Rumour(gossip,mailbox.Self,mailbox.Self)
+                        timer.Start()
                     else
                         actorList <- [ for i in 1 .. numNodes do yield (spawn system ("Actor_" + string (i))) Pushsum]
                         actorList |> List.iter(fun node -> node <! BuildNetwork(topology,mailbox.Self,actorList))
                         let selectedActor = actorList.[r.Next(1,numNodes)]
                         selectedActor <! Receive(selectedActor.Path.Name.Split('_').[1] |> float,1.0,mailbox.Self,mailbox.Self)
-
+                        timer.Start()
             |   Terminate(termMsg,actor) -> 
                     //Console.WriteLine termMsg
                     if termMsg = "Down" then
                         if not (List.contains actor dead) then
+                            //Console.WriteLine ("Terminated " + actor.ToString())
                             dead <- actor :: dead
                             actorList |> List.iter(fun node -> node <! Remove(actor) )   
                             Console.WriteLine dead.Length
@@ -276,8 +286,9 @@ let Supervisor (mailbox: Actor<_>) =
                     if termMsg = "Done" && actor = mailbox.Self then
                         mailbox.Context.System.Terminate() |> ignore
                         system.Terminate() |> ignore
+                        time <- timer.ElapsedMilliseconds |> int
                     //     // mailbox.Context.System.Terminate() |> ignore                                
-                               
+
 
 
             |   _ -> ignore()
@@ -287,5 +298,6 @@ let Supervisor (mailbox: Actor<_>) =
 
 let supervisor = spawn system "supervisor" Supervisor
 supervisor <! Begin("Begin")
-#time "on"
+//#time "on"
 system.WhenTerminated.Wait()
+Console.WriteLine ("Time taken to Converge " + time.ToString())
